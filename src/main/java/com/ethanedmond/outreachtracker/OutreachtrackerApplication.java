02d086c8fa8d51c63c1f919d93d403a6bf17a1bd
Ethan Edmond <eedmond@uccs.edu>
2021-10-17 22:52:15 -0600

    Started a spring boot project with intellij

---
+++ src/main/java/com/ethanedmond/outreachtracker/OutreachtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.ethanedmond.outreachtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutreachtrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutreachtrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ethanedmond/outreachtracker/OutreachtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ethanedmond.outreachtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutreachtrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
