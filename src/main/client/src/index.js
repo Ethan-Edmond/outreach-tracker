b94cd1f6660114610ae14f16889e15a814647de2
Ethan Edmond <eedmond@uccs.edu>
2021-10-24 18:03:40 +0000

    Added basic routes, needs secure Route

--- src/main/client/src/index.js
+++ src/main/client/src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Register from './components/Register';
+import Login from './components/Login';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route path='/register'>
+          <Register/>
+        </Route>
+        <Route path='/login'>
+          <Register/>
+        </Route>
+        <Route path='/'>
+          <App/>
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
