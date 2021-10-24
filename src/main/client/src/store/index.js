a11a8c74e407e604cbb78fefcf29ceb4af8bc232
Ethan Edmond <eedmond@uccs.edu>
2021-10-24 22:18:00 +0000

    Setup redux-thunk

--- src/main/client/src/store/index.js
+++ src/main/client/src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import loginReducer from './loginReducer';
 import jobReducer from './jobReducer';
+import thunk from 'redux-thunk';
 
 export default configureStore({
     reducer: {
         login: loginReducer,
         jobs: jobReducer
-    }
+    },
+    middleware: [thunk]
 });
