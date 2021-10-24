0ec68ad3c95f2521a8648ad6fc1afd9c6a789b05
Ethan Edmond <eedmond@uccs.edu>
2021-10-24 21:56:27 +0000

    Added some more skeletons to jobReducer

--- src/main/client/src/store/jobReducer.js
+++ src/main/client/src/store/jobReducer.js
@@ -12,9 +12,38 @@ export const jobSlice = createSlice({
             //   })
             //   .catch(err => actually handle error)
             return [...state, action.payload];
+        },
+        add: (state, action) => {
+            // axios.post('/jobs')
+            // .then(res => blablabla)
+            // .catch(err => blablabla)
+            return [...state, action.payload];
+        },
+        edit: (state, action) => {
+            // axios call, then set state
+            // axios.put('/jobs/{action.payload.id}')
+            //   .then(res => bla bla bla change state)
+            //   .catch(err => bla bla bla handle error)
+            // the payload should be the changed job, which should have an Id property
+            return state.map((job) => {
+                if (job.id === action.payload.id) {
+                    return {
+                        ...job,
+                        ...action.payload
+                    };
+                } else {
+                    return job;
+                }
+            })
+        },
+        remove: (state, action) => {
+            // TODO get this working off axios and updating backend
+            return state.filter((job) => {
+                return job.id === action.payload;
+            })
         }
     }
 })
 
-export const { initialize } = jobSlice.actions;
+export const { initialize, add, edit, remove } = jobSlice.actions;
 export default jobSlice.reducers;
