7d4462ad309c7c4e9f1959d2ce1119aafc25a724
Ethan Edmond <eedmond@uccs.edu>
2021-10-24 20:51:06 +0000

    Cleaned up unused imports in SecureRoute

--- src/main/client/src/utils/SecureRoute.js
+++ src/main/client/src/utils/SecureRoute.js
@@ -2,11 +2,9 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Route, useHistory } from "react-router-dom";
 import {decode} from "jsonwebtoken";
-import { setToken } from "../store/loginReducer";
 
 function SecureRoute (props) {
     const {push} = useHistory();
-    const dispatch = useDispatch();
     const token = useSelector((state) => state.login.token);
     const isGood = decode(token);
     const reject = () => {
